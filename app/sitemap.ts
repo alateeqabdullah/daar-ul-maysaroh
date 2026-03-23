b70991b634c240251319f050e29ed732ecfb7166
Al'ateeq <alateeqabdullah36@gmail.com>
2026-03-24 00:12:04 +0100

    hmmm

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
-    {   url: 'https://almaysaroh.com/',
+    {   url: 'https://almaysaroh.com',
         lastModified: new Date(),
         changeFrequency: 'yearly',
         priority: 1
