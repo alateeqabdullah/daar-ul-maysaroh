b7dd554d817d13cb126a002a7fae2509109ee437
Al'ateeq <alateeqabdullah36@gmail.com>
2026-03-24 23:17:17 +0100

    hmmm

--- app/robots.ts
+++ app/robots.ts
@@ -1,24 +1,19 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: [
-                '/admin',
-                '/dashboard',
-               
-               '/api',
-                '/login',
-                '/register',
-                '/password-reset',
-                '/thank-you',
-                '/404',
-                '/500',
-                
-            ]
-        },
-        sitemap: 'https://almaysaroh.com/sitemap.xml',
-    };
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/api/",
+        "/dashboard/",
+        "/admin/",
+        "/portal/",
+        "/pending",
+        "/account-status",
+      ],
+    },
+    sitemap: "https://almaysaroh.com/sitemap.xml",
+  };
 }
