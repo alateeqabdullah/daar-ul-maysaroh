51d2b463a2fa500fd5b692355a5e2afeff627ad4
Al'ateeq <alateeqabdullah36@gmail.com>
2026-03-24 16:20:05 +0100

    hmmm

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,25 +2,72 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
-    {   url: 'https://almaysaroh.com',
+      {
+        url: "https://almaysaroh.com",
         lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 1
-    },
-    {   url: 'https://almaysaroh.com/about',
+        changeFrequency: "yearly",
+        priority: 1,
+      },
+      {
+        url: "https://almaysaroh.com/about",
         lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 0.8
-    },
-    {   url: 'https://almaysaroh.com/courses',
+        changeFrequency: "yearly",
+        priority: 0.8,
+      },
+      {
+        url: "https://almaysaroh.com/courses",
         lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.9
-    },
-    {   url: 'https://almaysaroh.com/contact',
+        changeFrequency: "monthly",
+        priority: 0.9,
+      },
+      {
+        url: "https://almaysaroh.com/contact",
         lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 0.8
-    },
-];
+        changeFrequency: "yearly",
+        priority: 0.8,
+      },
+      {
+        url: "https://almaysaroh.com/methodology",
+        lastModified: new Date(),
+        changeFrequency: "yearly",
+        priority: 0.8,
+      },
+      {
+        url: "https://almaysaroh.com/legal",
+        lastModified: new Date(),
+        changeFrequency: "yearly",
+        priority: 0.8,
+      },
+      {
+        url: "https://almaysaroh.com/privacy",
+        lastModified: new Date(),
+        changeFrequency: "yearly",
+        priority: 0.8,
+      },
+      {
+        url: "https://almaysaroh.com/faq",
+        lastModified: new Date(),
+        changeFrequency: "yearly",
+        priority: 0.8,
+      },
+      {
+        url: "https://almaysaroh.com/login",
+        lastModified: new Date(),
+        changeFrequency: "yearly",
+        priority: 0.8,
+      },
+      {
+        url: "https://almaysaroh.com/admissions",
+        lastModified: new Date(),
+        changeFrequency: "yearly",
+        priority: 0.8,
+      },
+      {
+        url: "https://almaysaroh.com/pricing",
+        lastModified: new Date(),
+        changeFrequency: "yearly",
+        priority: 0.8,
+      },
+      
+    ];
 }
